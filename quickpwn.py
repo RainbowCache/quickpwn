6ee5adf4ad66c77d6a83ad6ea23a50a2882a2743
NyanNyanKoneko <statickate@gmail.com>
2023-02-12 15:22:06 -0800

    Can now scan.

--- quickpwn.py
+++ quickpwn.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+from autopwn_suite.api import AutoScanner
+
+
 def main():
     print("Hello.")
+
+    with open("IPS_FIRING_RANGE.TXT", "r") as ips:
+        for ip in ips:
+            scanner = AutoScanner()
+            ip = ip.strip()
+            try:
+                results = scanner.scan(ip)
+                print(results)
+            except:
+                print("Host scan failed. Are you running as root?")
 
 
 if __name__ == "__main__":
