eb763de03a949b38868ebf621f3581529a634790
NyanNyanKoneko <statickate@gmail.com>
2023-02-12 17:06:33 -0800

    More options.

--- quickpwn.py
+++ quickpwn.py
@@ -2,19 +2,45 @@
 
 from autopwn_suite.api import AutoScanner
 
+# Globals / Options
+api_key = None
+host_timeout = 0
+scan_speed = 5
+os_scan = True
+scan_vulns = True
+nmap_args = None
+debug_scan = True
+
+
+def scan_host(ip: str):
+    global host_timeout, scan_speed, api_key, os_scan, scan_vulns, nmap_args, debug_scan
+
+    scanner = AutoScanner()
+
+    try:
+        results = scanner.scan(ip, host_timeout=host_timeout, scan_speed=scan_speed, apiKey=api_key,
+                               os_scan=os_scan, scan_vulns=scan_vulns, nmap_args=nmap_args, debug=debug_scan)
+        print(results)
+        scanner.save_to_file(ip + "_results.json")
+    except Exception as e:
+        print(e)
+        print(e.args)
+        print("Host scan failed. Are you running as root?")
+
 
 def main():
-    print("Hello.")
+    global api_key
+
+    try:
+        with open("api.txt", "r") as api_file:
+            api_key = api_file.readline().strip()
+    except:
+        print("Unable to load api key.")
 
     with open("IPS_FIRING_RANGE.TXT", "r") as ips:
         for ip in ips:
-            scanner = AutoScanner()
             ip = ip.strip()
-            try:
-                results = scanner.scan(ip)
-                print(results)
-            except:
-                print("Host scan failed. Are you running as root?")
+            scan_host(ip)
 
 
 if __name__ == "__main__":
